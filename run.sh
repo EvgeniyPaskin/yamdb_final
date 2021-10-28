a719d7d942d46a89f9124061704c8c1a7459bbe2
Evgeniy Paskin <evgeniy.paskin+github@gmail.com>
2021-10-28 09:14:48 +0200

    copied yaml dockerfile

--- run.sh
+++ run.sh
@@ -1,9 +1,9 @@
-rm -f /app/pytest.ini
-rm -rf /app/tests
+rm -f /code/pytest.ini
+rm -rf /code/tests
 
-cp pytest.ini /app/pytest.ini
-cp -a tests/ /app/tests
+cp pytest.ini /code/pytest.ini
+cp -a tests/ /code/tests
 
-cd /app
+cd /code
 pip3 install -r requirements.txt
 pytest --tb=line 1>&2
